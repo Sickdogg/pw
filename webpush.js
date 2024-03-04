1a46cf68b83a7ce6efb035c2919a01a9406f3288
DESKTOP-KC4E7OT\sickdog <q2382285@gmail.com>
2024-03-04 17:16:18 +0800

    add

--- webpush.js
+++ webpush.js
@@ -19,12 +19,12 @@ webpush.setVapidDetails(
 // This is the same output of calling JSON.stringify on a PushSubscription
 const pushSubscription = {
   endpoint:
-    "https://wns2-ln2p.notify.windows.com/w/?token=BQYAAADkfTz9gDwoeJ4%2f0ZA2WbgUfdxfURhQnnGRYiCoaM9tVoi5xVAo4hsAXq55jlxBeK4JEwzdKqdxVIS7tZYBp5dLqapJRL41P5jDrFQtAOaf%2fbXWFRz5hQPcYP2b9QRN4U3etCBsZcHODb53tDcNbhuEIkmTA6k6Oj2W46D3BQ%2bx4wtv3EW6HFk8WuK8Sg%2btaXVEeXCAioUAIUTmbaz4rCsVP7VPG%2bhovKjgea2IVA7j15U7XlMcJtwTo26YS%2bX65WQf%2bLQlgpXws4O8E1cccQCLr3fzUXrJBc1qTbTS9Avd9BALgFcX29bDCp7MqiGpbsIZN%2fKHjelC6b39Ug78uB1W",
+    "https://fcm.googleapis.com/fcm/send/c1ew00qGeYk:APA91bEyl05ElPTcbfif9ApiksdERhhE6ViahdIq-ex26dKG8lQWIn4E7Z3ibeW4Y1QW1FWmcKNECxvXl-K3rb050uoQDNNayGKWgCGnwMywhnpY1dexKsOC-W_jY-2YS4s4kEGV22M0",
   expirationTime: null,
   keys: {
     p256dh:
-      "BFDtBwFczbV27GJtZLiHlifPv5MRwjwHkBiY75WRjXnQeiU_uRb7t11Fq0Vmn00QXIW9KT5uKQ5g2D6I1kCTx6g",
-    auth: "uVjWowi00JahOR4PXIP-7g",
+      "BNJCno6ODTrlqPVkaVhwft1tLpeEUeZBzOqrtvvmQAEC7mGLUVKrDkYyCBy5tauGXVSSjKupulZH_vmu6w8-PGg",
+    auth: "BPElDxL6F0dIs53q4zTFUw",
   },
 };
 
