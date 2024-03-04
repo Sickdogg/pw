47e6d708c527f1ae2c94c3f686b09d56fdba3dbb
DESKTOP-KC4E7OT\sickdog <q2382285@gmail.com>
2024-03-04 19:01:40 +0800

    add

--- sw.js
+++ sw.js
@@ -1,4 +1,21 @@
-self.addEventListener("push", (e) => {
+// self.addEventListener("push", (e) => {
+//   let options = {
+//     body: "This not was generated from a push !",
+//     icon: "images/example.png",
+//     vibrate: [100, 50, 100],
+//     data: {
+//       dateOfArrival: Date.now(),
+//       primarykey: "2",
+//     },
+//     actions: [
+//       { action: "example", title: "Explore", icon: "images/checkmark.png" },
+//       { action: "close", title: "Close", icon: "images/xmark.png" },
+//     ],
+//   };
+//   e.waitUntil(self.registration.showNotification("Hello world!", options));
+// });
+
+setInterval(()=>{
   let options = {
     body: "This not was generated from a push !",
     icon: "images/example.png",
@@ -13,4 +30,4 @@ self.addEventListener("push", (e) => {
     ],
   };
   e.waitUntil(self.registration.showNotification("Hello world!", options));
-});
+},5000)
