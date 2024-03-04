248d45753edfad83711fd9498ec33733e37fef68
DESKTOP-KC4E7OT\sickdog <q2382285@gmail.com>
2024-03-04 19:10:05 +0800

    add

--- sw.js
+++ sw.js
@@ -1,21 +1,4 @@
-// self.addEventListener("push", (e) => {
-//   let options = {
-//     body: "This not was generated from a push !",
-//     icon: "images/example.png",
-//     vibrate: [100, 50, 100],
-//     data: {
-//       dateOfArrival: Date.now(),
-//       primarykey: "2",
-//     },
-//     actions: [
-//       { action: "example", title: "Explore", icon: "images/checkmark.png" },
-//       { action: "close", title: "Close", icon: "images/xmark.png" },
-//     ],
-//   };
-//   e.waitUntil(self.registration.showNotification("Hello world!", options));
-// });
-
-setInterval(()=>{
+self.addEventListener("push", (e) => {
   let options = {
     body: "This not was generated from a push !",
     icon: "images/example.png",
@@ -29,5 +12,22 @@ setInterval(()=>{
       { action: "close", title: "Close", icon: "images/xmark.png" },
     ],
   };
-  e.waitUntil(self.registration.showNotification("Hello!", options));
-},5000)
+  e.waitUntil(self.registration.showNotification("Hello world!", options));
+});
+
+// setInterval(()=>{
+//   let options = {
+//     body: "This not was generated from a push !",
+//     icon: "images/example.png",
+//     vibrate: [100, 50, 100],
+//     data: {
+//       dateOfArrival: Date.now(),
+//       primarykey: "2",
+//     },
+//     actions: [
+//       { action: "example", title: "Explore", icon: "images/checkmark.png" },
+//       { action: "close", title: "Close", icon: "images/xmark.png" },
+//     ],
+//   };
+//   e.waitUntil(self.registration.showNotification("Hello!", options));
+// },5000)
